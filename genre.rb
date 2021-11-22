f4b6dc625af52d71b2dda19b81c593b17e8e7d99
Juliana Ifionu <julieifionu@gmail.com>
2021-11-22 15:01:10 +0100

    Add attribute's setter and reader to class properties

--- genre.rb
+++ genre.rb
@@ -1,4 +1,7 @@
 class Genre
+  attr_accessor :name
+  attr_reader :items
+
   def initialize(name)
     @id = Random.rand(1..1000)
     @name = name
--- music_album.rb
+++ music_album.rb
@@ -1,6 +1,9 @@
 require_relative './item'
 
 class MusicAlbum < Item
+  attr_accessor :publish_date, :on_spotify
+  attr_reader :archived
+
   def initialize(publish_date, archived, on_spotify: false)
     super(publish_date, archived)
     @on_spotify = on_spotify
