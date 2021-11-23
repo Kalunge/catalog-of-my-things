b0b0a85b781242e021991f4cdf84c13d0b28c7d8
Daniel M. Matongo <m_matongo@icloud.com>
2021-11-23 12:06:16 +0200

    update game.rb

--- game.rb
+++ game.rb
@@ -1,16 +1,16 @@
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+  attr_accessor :multiplayer, :last_played_at, :name, :publish_date
 
-  def initialize(multiplayer, last_played_at, *args)
-    super(*args)
+  def initialize(publish_date:, name: 'Unknown', archived: false, last_played_at: nil, multiplayer: false)
+    super(publish_date: publish_date, archived: archived, name: name)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
 
   def can_be_archived?
-    (Time.now.year - @last_played_at.year) > 2
+    super && (Time.now.year - @last_played_at.year) > 2
   end
 
   private :can_be_archived?
@@ -22,13 +22,15 @@ class Game < Item
   def to_json(*args)
     {
       JSON.create_id => self.class.name,
+      'name' => @name,
       'multiplayer' => @multiplayer,
+      'published_date' => @publish_date.strftime('%Y/%m/%d %k:%M'),
       'last_played_at' => @last_played_at
     }.merge(super).to_json(*args)
   end
 
   def self.json_create(obj)
-    game = new(obj['multiplayer'], obj['last_played_at'], Time.parse(obj['publish_date']))
+    game = new(obj['name'], obj['multiplayer'], obj['published_date'], obj['last_played_at'])
     game.id = obj['id']
     game
   end
