6177a80b2869a1e2648d680357a34b50f85ab1c7
kalunge <muthomititus+github@gmail.com>
2021-11-23 11:07:23 +0100

    Update handlers

---
+++ handlers/book_label_handler.rb
@@ -0,0 +1,33 @@
+module BookLabelHandlers
+  def list_all_books
+    @books.each_with_index do |book, index|
+      if @books.any?
+        puts "#{index}) name: #{book.name}, publisher:
+        #{book.publisher}, published_year:#{book.publish_date} coverstate: #{book.cover_state}"
+      else
+        puts 'You have not added any books yet'.upcase
+      end
+    end
+  end
+
+  def list_all_labels
+    @labels.each_with_index do |label, index|
+      puts "#{index}) Title: #{label.title}, Color: #{label.color}" if @labels.any?
+      puts 'You have not added any labels yet'.upcase
+    end
+  end
+
+  def add_book
+    print 'name: '
+    name = gets.chomp
+    print 'publisher: '
+    publisher = gets.chomp
+    print 'date of publish: '
+    publish_date = gets.chomp
+    print 'cover state: '
+    cover_state = gets.chomp
+    book = Book.new(name: name, cover_state: cover_state, publisher: publisher, publish_date: publish_date)
+    @books << book
+    message
+  end
+end
