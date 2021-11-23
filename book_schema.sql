1f40dd78984999f5574934f1d4cb888858fd3d36
kalunge <muthomititus+github@gmail.com>
2021-11-23 14:46:48 +0100

    Add source relationship

--- book_schema.sql
+++ book_schema.sql
@@ -7,11 +7,13 @@ CREATE TABLE books(
   genre_id      INT,
   author_id     INT,
   label_id      INT,
+  source_id      INT,
   archived      BOOLEAN,
   PRIMARY KEY(id)
   FOREIGN KEY(genre_id) REFERENCES genre(id),
   FOREIGN KEY(author_id) REFERENCES author(id),
   FOREIGN KEY(label_id) REFERENCES label(id)
+  FOREIGN KEY(source_id) REFERENCES source(id)
 );
 
 CREATE TABLE labels(
