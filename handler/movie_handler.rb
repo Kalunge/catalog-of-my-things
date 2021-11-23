894190fe2bdd8116b829dbb685308bf06fc51303
ridaarif98 <ridaarif16@gmail.com>
2021-11-23 11:55:00 +0100

    Add source and create method to display all sources

--- handler/movie_handler.rb
+++ handler/movie_handler.rb
@@ -5,17 +5,22 @@ class Movie_Handler
     attr_accessor :movie
     def initialize()
         @movies= []
+        @sources = [] 
     end
 
     def add_movie
         print 'Movie name: '
         name = gets.chomp
+        print 'Movie Source: '
+        source_name= gets.chomp
         print 'Published Date: '
         publish_date = gets.chomp
         print 'Silet? [Y/N]: '
         silet = gets.chomp != 'n'
         print 'Archived: '
         archived = gets.chomp != 'n'
+
+        @sources.push(Source.new(source_name))
         @movies.push(Movie.new(publish_date: publish_date, silet: silet, archived: archived, name: name))
         puts('Successfully added movie!')
         choice_selection
@@ -27,9 +32,15 @@ class Movie_Handler
         choice_selection
     end
 
+    def all_soucrce
+        puts 'All sources are: '
+        @sources.each { |source| puts "Name: '#{source.name}', ID: #{source.id}" }
+        choice_selection
+    end
+
     def choice_selection
         puts 'Please Choose an option by entrin a number:'
-        choose_options = ['1- Add movie',  '2- List of movies', '3- Exit']
+        choose_options = ['1- Add movie',  '2- List of movies', '3- List all sources', '4-Exit']
         choose_options.map { |choose| puts choose }
         options = gets.chomp.to_i
         case options
@@ -37,6 +48,8 @@ class Movie_Handler
             add_movie
         when 2
             all_movie
+        when 3
+            all_soucrce
         else
             puts 'Thanks for using this app!'
         end
