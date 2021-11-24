6daf5759bf499e45bed8ee3a4d67ef9375008057
Juliana Ifionu <julieifionu@gmail.com>
2021-11-24 17:13:45 +0100

    Create music_albums and genres schema

--- schema.sql
+++ schema.sql
@@ -41,3 +41,16 @@ CREATE TABLE labels(
   PRIMARY KEY(id)
 );
 
+CREATE TABLE music_albums(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  on_spotify BOOLEAN,
+  FOREIGN KEY(id) REFERENCES item(id)
+);
+
+CREATE TABLE genres(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(255)
+  PRIMARY KEY(id)
+);
+
+
