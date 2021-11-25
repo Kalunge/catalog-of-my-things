b1164248cca52ae02b44db2cbd139812251d15c8
Daniel M. Matongo <m_matongo@icloud.com>
2021-11-25 12:22:13 +0200

    add schema

--- schema.sql
+++ schema.sql
@@ -41,3 +41,17 @@ CREATE TABLE labels(
   PRIMARY KEY(id)
 );
 
+CREATE TABLE author(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  PRIMARY KEY(id)
+);
+
+
+CREATE TABLE games(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  multiplayer BOOLEAN,
+  last_played_at DATE,
+  FOREIGN KEY(id) REFERENCES item(id)
+);
